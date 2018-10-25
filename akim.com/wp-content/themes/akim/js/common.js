1a9fe4f749a302083fdf2222207fb9bf49354f18
佐藤正康 <satohmsys@gmail.com>
2018-10-25 23:25:31 +0900

    add files

---
+++ akim.com/wp-content/themes/akim/js/common.js
@@ -0,0 +1,42 @@
+$( function(){
+
+    var $w = $( window ),
+        $ww = $w.width(),
+        $wh = $w.height(),
+        $breakpoint_mobile = 768,
+        $scrollVal,
+        $scrollBottom;
+
+    $w.on( 'load resize', function( e ){
+        e.stopPropagation();
+
+        $ww = $( window ).width();
+    });
+
+    /**
+     * global nav toggle
+     */
+    if ($ww < $breakpoint_mobile ){
+        navHandling($('.navtoggle--open') ); 
+        navHandling($('.navtoggle--close') ); 
+    }
+
+
+});
+
+
+/**
+ * ナビの開閉ハンドリング
+ * @param { jquery obj } $fire 
+ */
+function navHandling($fire) {
+    $fire.on('click', function (e) {
+        e.stopPropagation();
+        // console.log($(this))
+
+        var $b = $('body');
+        // console.log($b.hasClass('-is-navOpen'));
+        $b.hasClass('-is-navOpen') ? $b.removeClass('-is-navOpen') : $b.addClass('-is-navOpen');
+        // $b.toggleClass('-is-navOpen');
+    });
+}
