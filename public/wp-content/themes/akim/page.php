94e87c973f4733d74f8be8345fb69a6afe7d08c7
satohmsys_Home <satohmsys@gmail.com>
2018-11-06 23:02:23 +0900

    add 通常固定ページ

--- public/wp-content/themes/akim/page.php
+++ public/wp-content/themes/akim/page.php
@@ -1,41 +1,39 @@
 <?php
 /**
- * The template for displaying all pages
+ * 固定ページベース  template file
  *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages
- * and that other 'pages' on your WordPress site may use a
- * different template.
- *
- * @link https://codex.wordpress.org/Template_Hierarchy
+ * If the user has selected a static page for their homepage, this is what will
+ * appear.
+ * Learn more: https://codex.wordpress.org/Template_Hierarchy
  *
  * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since 1.0
- * @version 1.0
+ * @subpackage Akim corpolate
  */
 
 get_header(); ?>
+<?php
+	if( have_posts() ){
+		while( have_posts() ){
+			the_post();			
+?>
+<!-- ***** mv -->
+<section class="subpageMv js-effect">
+	<div class="wrap">
+		<h1 class="subpageMv__copy">
+			<span class="mask"><span class="mask__element"><?php the_title(); ?></span></span>
+		</h1>
+	</div>
+</section>
+<!-- ///// -->
+<?php
+	get_template_part( '_inc/template-parts/navigation/breadcrumb');
+?>
+<main class="mainContent">
+	<?php the_content(); ?>
+</main>       
+<?php
+		}
+	}
+?>
+<?php get_footer();?>
 
-<div class="wrap">
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
-
-			<?php
-			while ( have_posts() ) : the_post();
-
-				get_template_part( 'template-parts/page/content', 'page' );
-
-				// If comments are open or we have at least one comment, load up the comment template.
-				if ( comments_open() || get_comments_number() ) :
-					comments_template();
-				endif;
-
-			endwhile; // End of the loop.
-			?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
-</div><!-- .wrap -->
-
-<?php get_footer();
