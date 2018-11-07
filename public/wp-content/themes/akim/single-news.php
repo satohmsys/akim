a900a19b729b1f94b18cb36b107d995bc5f35c79
satohmsys_Home <satohmsys@gmail.com>
2018-11-07 23:17:23 +0900

    update news シングルページ　レイアウト変更

--- public/wp-content/themes/akim/single-news.php
+++ public/wp-content/themes/akim/single-news.php
@@ -22,69 +22,70 @@ get_header(); ?>
 ?>
 <main class="mainContent">
     <section class="section section--single">
-        <?php
-        while( have_posts() ){
-            the_post();
-        ?>
-        <article class="newstopic section__child postArea">
-            <header class="postArea__index newstopic__index wrap">
-                <h2 class="postArea__index__text newstopic__index__text">
-                    <?php the_title(); ?>
-                </h2>
-                <div class="postArea__index__date newstopic__index__date">
-                    <time><?php the_time( 'Y.m.d' ); ?></time>
-                </div>
-            </header>
-            <div class="newstopic__content postArea__content wrap">
-                <?php the_content(); ?>
-            </div>
-        </article>
-        <?php } ?>
-        <aside class="section__child otherNews wrap">
-            <header class="section__index">
-                <h3 class="section__index__text">その他のニュース</h3>
-            </header>
-            <?php
-                $arg = array(
-                    'post_type' => 'news',
-                    'posts_per_page' > 5,
-                    'orderby' => 'post_date',
-                    'order' => 'desc'
-                );
-                $wpq = new WP_Query( $arg );
-                if( $wpq->have_posts() ){                
-            ?>
-                <ul class="news">
+        <div class="wrap">
+            <div class="column">
                 <?php
-                    while( $wpq->have_posts() ){
-                        $wpq->the_post();
+                while( have_posts() ){
+                    the_post();
                 ?>
-                <li class="news__item">
-                    <article>
-                        <header class="news__item__index">
-                            <h2 class="news__item__index__text">
-                                <?php the_title(); ?>
-                            </h2>
-                            <div class="news__item__index__date">
-                                <time><?php the_time( 'Y.m.d' ); ?></time>
-                            </div>
-                        </header>
-                        <div class="news__item__description">
-                            <?php the_excerpt(); ?>
+                <article class="column__main section__child newstopic postArea">
+                    <header class="postArea__index newstopic__index">
+                        <h2 class="postArea__index__text newstopic__index__text">
+                            <?php the_title(); ?>
+                        </h2>
+                        <div class="postArea__index__date newstopic__index__date">
+                            <time><?php the_time( 'Y.m.d' ); ?></time>
                         </div>
-                        <div class="button"><a href="<?php the_permalink(); ?>">お知らせを読む</a></div>
-                    </article>
-                </li>			
-                <?php
-                    }
-                    wp_reset_postdata();
-                ?>
-                </ul>
-                <?php 		
-                    }
-                ?>
+                    </header>
+                    <div class="newstopic__content postArea__content">
+                        <?php the_content(); ?>
+                    </div>
+                </article>
+                <?php } ?>
+                <aside class="column__sub otherNews">
+                    <h3 class="subColumnSection__index">
+                        その他のニュース
+                    </h3>
+                    <?php
+                        $arg = array(
+                            'post_type' => 'news',
+                            'posts_per_page' => 4,
+                            'orderby' => 'post_date',
+                            'order' => 'desc'
+                        );
+                        $wpq = new WP_Query( $arg );
+                        if( $wpq->have_posts() ){                
+                    ?>
+                        <ul class="news">
+                        <?php
+                            while( $wpq->have_posts() ){
+                                $wpq->the_post();
+                        ?>
+                        <li class="news__item">
+                            <article>
+                                <header class="news__item__index">
+                                    <h2 class="news__item__index__text">
+                                        <?php the_title(); ?>
+                                    </h2>
+                                    <div class="news__item__index__date">
+                                        <time><?php the_time( 'Y.m.d' ); ?></time>
+                                    </div>
+                                </header>
+                                <div class="button"><a href="<?php the_permalink(); ?>">お知らせを読む</a></div>
+                            </article>
+                        </li>			
+                        <?php
+                            }
+                            wp_reset_postdata();
+                        ?>
+                        </ul>
+                        <?php 		
+                            }
+                        ?>
+                    </div>
+                </aside>
             </div>
-        </aside>
+        </div>
     </section>
 </main>       
 <?php get_footer();?>
