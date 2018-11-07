cd8f70c0c243a3cd81c2540c24c8ec697c8f7b71
satohmsys_Home <satohmsys@gmail.com>
2018-11-07 23:23:03 +0900

    add template
    下層ページ用のMV

---
+++ public/wp-content/themes/akim/_inc/template-parts/header/subpage-mv.php
@@ -0,0 +1,12 @@
+<?php
+    $subpagetitle = $subpagetitle ? $subpagetitle :  the_title();
+?>
+<!-- ***** mv -->
+<section class="subpageMv js-effect">
+	<div class="wrap">
+		<h1 class="subpageMv__copy">
+			<span class="mask"><span class="mask__element"><?php echo $subpagetitle ?></span></span>
+		</h1>
+	</div>
+</section>
+<!-- ///// -->
