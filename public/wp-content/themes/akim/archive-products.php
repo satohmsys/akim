39f0cbd639dbf39a2b354d3f725c8c602d0fa661
satohmsys_Home <satohmsys@gmail.com>
2018-11-12 20:16:30 +0900

    update 製品情報アーカイブ 固定部分追記（リンク未挿入）

--- public/wp-content/themes/akim/archive-products.php
+++ public/wp-content/themes/akim/archive-products.php
@@ -21,6 +21,51 @@ get_header(); ?>
 	get_template_part( '_inc/template-parts/navigation/breadcrumb');
 ?>
 <main class="mainContent">
-	<?php get_template_part( '_inc/template-base/page-productCat'); ?>
+<section class="section section--products">
+	<div class="wrap">
+		<section class="section__child">
+			<?php get_template_part( '_inc/template-parts/post/productlist'); ?>	
+		</section>	
+		<section class="section__child">
+			<ul class="productBanner">
+				<li class="productBanner__item">
+					<a href="">
+						<div class="button button-secondary">
+							<span>
+								<header class="productBanner__item__index">
+									高速生産ライン
+								</header>
+								<p class="productBanner__item__description">
+									タクト0.6secを実現
+								</p>
+							</span>
+						</div>
+						<figure class="productBanner__item__img">
+							<img src="<?php echo AKIM_IMG_URI; ?>products/link-line.jpg" alt="">
+						</figure>
+					</a>
+				</li>
+				<li class="productBanner__item productBanner--movie">
+					<a href="https://www.youtube.com/watch?v=_sI_Ps7JSEk" data-fancybox>
+						<div class="button button-secondary">
+							<span>
+								<header class="productBanner__item__index">
+									MOVIE
+								</header>
+								<p class="productBanner__item__description">
+									Akimの機械技術を一部紹介
+								</p>
+							</span>
+						</div>
+						<figure class="productBanner__item__img">
+							<img src="<?php echo AKIM_IMG_URI; ?>products/link-movie.jpg" alt="">
+						</figure>
+						<i class="icon--play"></i>
+					</a>
+				</li>
+			</ul>
+		</section>
+	</div>
+</section>
 </main>       
 <?php get_footer();?>
