9a24fd2e29fbacfdfde5fc4d622951d18ec17cf5
satohmsys_Home <satohmsys@gmail.com>
2018-11-12 15:58:20 +0900

    add SQLこのタイミング？

---
+++ sql/local.wp_comments.sql
@@ -0,0 +1,5 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_comments` VALUES
+(1,1,"WordPress コメントの投稿者","wapuu@wordpress.example","https://wordpress.org/","","2018-11-06 13:38:59","2018-11-06 04:38:59","こんにちは、これはコメントです。\nコメントの承認、編集、削除を始めるにはダッシュボードの「コメント画面」にアクセスしてください。\nコメントのアバターは「<a href=\"https://gravatar.com\">Gravatar</a>」から取得されます。",0,"1","","",0,0);
---
+++ sql/local.wp_users.sql
@@ -0,0 +1,5 @@
+/*!40101 SET NAMES binary*/;
+/*!40014 SET FOREIGN_KEY_CHECKS=0*/;
+/*!40103 SET TIME_ZONE='+00:00' */;
+INSERT INTO `wp_users` VALUES
+(1,"satohmsys","$P$Bnn3ss71K25PbomhMVcWoqZzAF44ZV0","satohmsys","satohmsys@gmail.com","","2018-11-06 04:38:59","",0,"satohmsys");
