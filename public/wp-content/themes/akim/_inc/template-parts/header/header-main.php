fd8649396560609599d63d2e7f32f1cb02e909a5
satohmsys_Home <satohmsys@gmail.com>
2018-11-12 17:19:01 +0900

    update template　ヘッダーナビ

--- public/wp-content/themes/akim/_inc/template-parts/header/header-main.php
+++ public/wp-content/themes/akim/_inc/template-parts/header/header-main.php
@@ -22,7 +22,25 @@
 					<i></i>
 					<span class="navtoggle__label">CLOSE</span>
 				</div>
-				<nav>
+				<?php if ( has_nav_menu( 'Header' ) ){ ?>
+					<nav>
+						<?php
+							wp_nav_menu( array(
+								'container' => 'ul',
+								'items_wrap' => '<ul class="%2$s">%3$s</ul>',
+								'menu' => null,
+								'container_id' => null,
+								'theme_location' => 'Header',
+								'menu_class'     => 'siteHeader__mainNav__wpnavmenu',
+								'depth'          => 1,
+								'link_before'    => '',
+								'link_after'     => '',
+							) );
+						?>
+						<div class="language"><a href=""><span class="language__label">Global</span><span>EN</span></a></div>
+					</nav>
+				<?php } ?>
+				<!-- <nav>
 					<ul>
 						<li ><a href="/page-archive-company.html">会社案内</a></li>
 						<li >
@@ -32,26 +50,12 @@
 						<li ><a href="/news.html">お知らせ</a></li>
 						<li ><a href="/page-contact.html">お問い合わせ</a></li>
 					</ul>
-					<div class="language"><a href=""><span class="language__label">Global</span><span>EN</span></a></div>
-				</nav>
+				</nav> -->
 			</div>
 			<div class="navtoggle navtoggle--open">
 				<span class="navtoggle__label">MENU</span>
 				<i></i>
 			</div>
 		</nav>
-        <?php if ( has_nav_menu( 'social' ) ) : ?>
-<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentyseventeen' ); ?>">
-	<?php
-		wp_nav_menu( array(
-			'theme_location' => 'social',
-			'menu_class'     => 'social-links-menu',
-			'depth'          => 1,
-			'link_before'    => '<span class="screen-reader-text">',
-			'link_after'     => '</span>' . twentyseventeen_get_svg( array( 'icon' => 'chain' ) ),
-		) );
-	?>
-</nav>
-<?php endif;?>
 	</div>
 </header>
