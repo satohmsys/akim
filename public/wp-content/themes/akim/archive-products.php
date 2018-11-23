1b60f4d60194bf8d1f5898ca03d10d09be9ee64f
佐藤正康 <satohmsys@gmail.com>
2018-11-23 19:37:50 +0900

    1121:製品アーカイブ文言修正

--- public/wp-content/themes/akim/archive-products.php
+++ public/wp-content/themes/akim/archive-products.php
@@ -21,51 +21,56 @@ get_header(); ?>
 	get_template_part( '_inc/template-parts/navigation/breadcrumb');
 ?>
 <main class="mainContent">
-<section class="section section--products">
-	<div class="wrap">
-		<section class="section__child">
-			<?php get_template_part( '_inc/template-parts/post/productlist'); ?>	
-		</section>	
-		<section class="section__child">
-			<ul class="productBanner">
-				<li class="productBanner__item">
-					<a href="<?php echo AKIM_PAGE_HIGHSPEEDLINE; ?>">
-						<div class="button button-secondary">
-							<span>
-								<header class="productBanner__item__index">
-									高速生産ライン
-								</header>
-								<p class="productBanner__item__description">
-									タクト0.6secを実現
-								</p>
-							</span>
-						</div>
-						<figure class="productBanner__item__img">
-							<img src="<?php echo AKIM_IMG_URI; ?>products/link-line.jpg" alt="">
-						</figure>
-					</a>
-				</li>
-				<li class="productBanner__item productBanner--movie">
-					<a href="https://www.youtube.com/watch?v=_sI_Ps7JSEk" data-fancybox>
-						<div class="button button-secondary">
-							<span>
-								<header class="productBanner__item__index">
-									MOVIE
-								</header>
-								<p class="productBanner__item__description">
-									Akimの機械技術を一部紹介
-								</p>
-							</span>
-						</div>
-						<figure class="productBanner__item__img">
-							<img src="<?php echo AKIM_IMG_URI; ?>products/link-movie.jpg" alt="">
-						</figure>
-						<i class="icon--play"></i>
-					</a>
-				</li>
-			</ul>
-		</section>
-	</div>
-</section>
+	<section class="subpageIntro">
+		<div class="wrap">
+			<strong>カメラモジュール・水晶デバイス・センサデバイスなど電子部品の生産設備を複合的な技術と視点で、<br>計画から開発・運用サポートまでのソリューション型製品として提供しています。</strong>
+		</div>
+	</section>	
+	<section class="section section--products">
+		<div class="wrap">
+			<section class="section__child">
+				<?php get_template_part( '_inc/template-parts/post/productlist'); ?>	
+			</section>	
+			<section class="section__child">
+				<ul class="productBanner">
+					<li class="productBanner__item">
+						<a href="<?php echo AKIM_PAGE_HIGHSPEEDLINE; ?>">
+							<div class="button button-secondary">
+								<span>
+									<header class="productBanner__item__index">
+										高速生産ライン
+									</header>
+									<p class="productBanner__item__description">
+										タクト0.6secを実現
+									</p>
+								</span>
+							</div>
+							<figure class="productBanner__item__img">
+								<img src="<?php echo AKIM_IMG_URI; ?>products/link-line.jpg" alt="">
+							</figure>
+						</a>
+					</li>
+					<li class="productBanner__item productBanner--movie">
+						<a href="https://www.youtube.com/watch?v=_sI_Ps7JSEk" data-fancybox>
+							<div class="button button-secondary">
+								<span>
+									<header class="productBanner__item__index">
+										MOVIE
+									</header>
+									<p class="productBanner__item__description">
+										Akimの機械技術を一部紹介
+									</p>
+								</span>
+							</div>
+							<figure class="productBanner__item__img">
+								<img src="<?php echo AKIM_IMG_URI; ?>products/link-movie.jpg" alt="">
+							</figure>
+							<i class="icon--play"></i>
+						</a>
+					</li>
+				</ul>
+			</section>
+		</div>
+	</section>
 </main>       
 <?php get_footer();?>
