c613e5da6c0c27923b109df70a47bde5e11749ad
佐藤正康 <satohmsys@gmail.com>
2019-02-14 01:37:53 +0900

    update php 製品情報パーツ　翻訳関数

--- public/wp-content/themes/akim/_inc/template-parts/post/productbanner-related.php
+++ public/wp-content/themes/akim/_inc/template-parts/post/productbanner-related.php
@@ -1,24 +1,24 @@
 <ul class="productBanner relatedProduct">
 <li class="productBanner__item relatedProduct__item">
     <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/detail-link-suisho-modal.png" data-caption='<div class="fancybox-caption__index">
-            水晶振動子外観・構造
+            <?php echo __('水晶振動子外観・構造', 'akim_tr'); ?>
         </div>'>							
         <figure class="relatedProduct__item__img">
             <img src="<?php echo AKIM_IMG_URI; ?>products/detail-link-suisho.jpg" alt="">
         </figure>
         <div class="button">
-            <span>水晶振動子外観・構造</span>
+            <span><?php echo __('水晶振動子外観・構造', 'akim_tr'); ?></span>
         </div>
     </a>
 </li>
 <li class="productBanner__item relatedProduct__item">
     <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/detail-link-crumpcareer-modal.png" data-caption='<div class="fancybox-caption__index">
-            <p>トレイ</p><p>クランプキャリア<br><strong>特許番号</strong>&nbsp;4895061,6068281</p></div>'>							
+            <?php echo __('<p>トレイ</p><p>クランプキャリア<br><strong>特許番号</strong>','akim_tr'); ?>&nbsp;4895061,6068281</p></div>'>							
         <figure class="relatedProduct__item__img">
             <img src="<?php echo AKIM_IMG_URI; ?>products/detail-link-crumpcareer.jpg" alt="">
         </figure>
         <div class="button">
-            <span>トレイ・クランプキャリア</span>
+            <span><?php echo __('トレイ・クランプキャリア', 'akim_tr');?></span>
         </div>
     </a>
 </li>
