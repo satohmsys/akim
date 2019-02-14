652bcc64e5cd2b04a5c883f36d954fea275b64f2
佐藤正康 <satohmsys@gmail.com>
2019-02-14 22:30:13 +0900

    init

---
+++ public/wp-content/themes/akim-child-ch/functions.php
@@ -0,0 +1,24 @@
+<?php
+
+
+/**
+ * CSS読み込み
+*/
+  function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('parent-style', 'contact'));
+  }
+  add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+
+
+/**
+ * 言語ファイル読み込み（ch）
+ * @link ： http://kwski.net/wordpress/1075/
+ * @link ： http://affinote.net/wordpress/1965/
+ * @link 翻訳関数について：https://php1st.com/1972
+ */
+add_action( 'after_setup_theme', 'akim_multilang_ch' );
+function akim_multilang_ch(){
+    load_child_theme_textdomain( 'akim_tr', get_stylesheet_directory() . '/lang' );
+}
+?>
