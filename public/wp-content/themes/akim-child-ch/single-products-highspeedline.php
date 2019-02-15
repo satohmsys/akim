2eebeb8c85daec649dc18c1dcb25c3a3baa2c878
佐藤正康 <satohmsys@gmail.com>
2019-02-15 14:25:38 +0900

    php 高速生産ライン翻訳

--- public/wp-content/themes/akim-child-ch/single-products-highspeedline.php
+++ public/wp-content/themes/akim-child-ch/single-products-highspeedline.php
@@ -14,135 +14,145 @@ get_header(); ?>
 <?php
     while( have_posts() ){
         the_post();
-?>    
+?>
 <!-- ***** mv -->
-<?php include('_inc/template-parts/header/subpage-mv.php'); ?>
+<?php include(dirname(__FILE__).'/../akim/_inc/template-parts/header/subpage-mv.php'); ?>
 <!-- ///// -->
 <?php
-	get_template_part( '_inc/template-parts/navigation/breadcrumb');
+	include( dirname(__FILE__).'/../akim/_inc/template-parts/navigation/breadcrumb.php');
 ?>
 <main class="mainContent">
-          <!-- ***** 製品一覧 -->
-        <section class="section section--lineup">
-            <div class="wrap">
-                <header class="section--lineup__index">
-                    <h2 class="section--lineup__index__text">
-                      소형수정진동작고속생산라인
-                    </h2>
-                    <p class="section--lineup__index__description">
-                    택트 타임0.6sec을 달성한 AKIM의 고속생산라인입니다.<br class="-no-sp">
-각 장비를 클릭하면 상세내용을 보실수 있습니다.
-                    </p>
-                </header>
-                <div class="lineupArea">
-                    <div class="lineup">
-                        <div class="lineup__item">
-                            <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/line-slider-8-modal.png" data-caption='<div class="fancybox-caption__index">
-                                마스크인서트장비
-                            </div><div class="fancybox-caption__description">W1100☓D1200☓H1700<br>수정 블랭크를 마스크에 삽입합니다.<br>패키지의 정열이재에도 대응가능</div>'>
-                                <figure class="lineup__item__img">
-                                    <img src="<?php echo AKIM_IMG_URI; ?>products/line-slider-8.png" width="200" alt="">
-                                    <figcaption class="lineup__item__name">마스크인서트장비</figcaption>
-                                </figure>
-                            </a>
-                        </div>                
-                        <div class="lineup__item">
-                            <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/line-slider-1-modal.png" data-caption='<div class="fancybox-caption__index">
-                                블랭크마운터
-                            </div><div class="fancybox-caption__description">"W1700☓D1400☓H1700 <br>세라믹패키지에 수정블랭크를 접착탑재합니다."</div>'>
-                                <figure class="lineup__item__img">
-                                    <img src="<?php echo AKIM_IMG_URI; ?>products/line-slider-1.png" width="200" alt="">
-                                    <figcaption class="lineup__item__name">블랭크마운터</figcaption>
-                                </figure>
-                            </a>
-                        </div>
-                        <div class="lineup__item">
-                            <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/line-slider-2-modal.png" data-caption='<div class="fancybox-caption__index">
-                                크린경화로
-                            </div><div class="fancybox-caption__description">W2800☓D1400☓H1700 <br>질소분위기내에서 접착제를 가열・경화합니다. <br>로내 크린도는 클래스100이하입니다.</div>'>
-                                <figure class="lineup__item__img">
-                                    <img src="<?php echo AKIM_IMG_URI; ?>products/line-slider-2.png" width="200" alt="">
-                                    <figcaption class="lineup__item__name">크린경화로</figcaption>
-                                </figure>
-                            </a>
-                        </div>
-                        <div class="lineup__item">
-                            <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/line-slider-3-modal.png" data-caption='<div class="fancybox-caption__index">
-                                F조 로더/언로더
-                            </div><div class="fancybox-caption__description">W2300☓D1300☓H1700<br>반송캐리어상의 트레이에 패키지를 이재합니다.<br>캐리어를 F조기에 반송합니다.                            </div>'>
-                                <figure class="lineup__item__img">
-                                    <img src="<?php echo AKIM_IMG_URI; ?>products/line-slider-3.png" width="200" alt="">
-                                    <figcaption class="lineup__item__name">F조 로더/언로더</figcaption>
-                                </figure>
-                            </a>
-                        </div>
-                        <div class="lineup__item">
-                            <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/line-slider-4-modal.png" data-caption='<div class="fancybox-caption__index">
-                                리드가용접장비
-                            </div><div class="fancybox-caption__description">"W1200☓D1000☓H1700<br>패키지에 리드를 가용접합니다."</div>'>
-                                <figure class="lineup__item__img">
-                                    <img src="<?php echo AKIM_IMG_URI; ?>products/line-slider-4.png" width="200" alt="">
-                                    <figcaption class="lineup__item__name">리드가용접장비</figcaption>
-                                </figure>
-                            </a>
-                        </div>
-                        <div class="lineup__item">
-                            <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/line-slider-5-modal.png" data-caption='<div class="fancybox-caption__index">
-                                진공어닐장비
-                            </div><div class="fancybox-caption__description">"W1100☓D1300☓H1700<br>실링전 패키지를 진공내에서 어닐처리를 실시합니다."</div>'>
-                                <figure class="lineup__item__img">
-                                    <img src="<?php echo AKIM_IMG_URI; ?>products/line-slider-5.png" width="200" alt="">
-                                    <figcaption class="lineup__item__name">진공어닐장비</figcaption>
-                                </figure>
-                            </a>
-                        </div>
-                        <div class="lineup__item">
-                            <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/line-slider-6-modal.png" data-caption='<div class="fancybox-caption__index">
-                                직교진공심용접장비
-                            </div><div class="fancybox-caption__description">"W1700☓D1200☓H1700<br>진공내에서 패키지의 심용접을 실시합니다."</div>'>
-                                <figure class="lineup__item__img">
-                                    <img src="<?php echo AKIM_IMG_URI; ?>products/line-slider-6.png" width="200" alt="">
-                                    <figcaption class="lineup__item__name">직교진공심용접장비</figcaption>
-                                </figure>
-                            </a>
-                        </div>
-                        <div class="lineup__item">
-                            <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/line-slider-7-modal.png" data-caption='<div class="fancybox-caption__index">
-                                선별장비
-                            </div><div class="fancybox-caption__description">"W800☓D900☓H1700<br>
-반송치구에서 제품을 취출하여 양품・불량품으로 분류합니다."</div>'>
-                                <figure class="lineup__item__img">
-                                    <img src="<?php echo AKIM_IMG_URI; ?>products/line-slider-7.png" width="200" alt="">
-                                    <figcaption class="lineup__item__name">선별장비</figcaption>
-                                </figure>
-                            </a>
-                        </div>
-                        <div class="lineup__item">
-                            <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/line-slider-9-modal.png" data-caption='<div class="fancybox-caption__index">
-                                온도특성검사장비
-                            </div><div class="fancybox-caption__description">"W1000☓D1000☓H1700<br>수정진동자의 전기특성검사, 레이저마킹 및<br>엠보스테이핑을 실시합니다."</div>'>
-                                <figure class="lineup__item__img">
-                                    <img src="<?php echo AKIM_IMG_URI; ?>products/line-slider-9.png" width="200" alt="">
-                                    <figcaption class="lineup__item__name">온도특성검사장비</figcaption>
-                                </figure>
-                            </a>
-                        </div>
-                        <div class="lineup__item">
-                            <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/line-slider-10-modal.png" data-caption='<div class="fancybox-caption__index">
-                                상온검사・마킹・테이핑장비
-                            </div><div class="fancybox-caption__description">"W1200☓D1000☓H1700<br>수정진동자의 전기특성검사, 레이저마킹 및 엠보스테이핑을 실시합니다."</div>'>
-                                <figure class="lineup__item__img">
-                                    <img src="<?php echo AKIM_IMG_URI; ?>products/line-slider-10.png" width="200" alt="">
-                                    <figcaption class="lineup__item__name">상온검사・마킹・테이핑장비</figcaption>
-                                </figure>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!-- /////  -->
-</main>       
+  <!-- ***** 製品一覧 -->
+  <section class="section section--lineup">
+    <div class="wrap">
+      <header class="section--lineup__index">
+        <h2 class="section--lineup__index__text">
+          高速自動生產線
+        </h2>
+        <p class="section--lineup__index__description">
+          AKIM的高速生產線，其生產節拍時間為0.6秒。<br class="-no-sp">
+          點擊每個設備以查看詳細信息
+        </p>
+      </header>
+      <div class="lineupArea">
+        <div class="lineup">
+          <div class="lineup__item">
+            <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/line-slider-8-modal.png"
+              data-caption='<div class="fancybox-caption__index">
+                                晶片上片機
+                            </div><div class="fancybox-caption__description">W1100☓D1200☓H1700<br>晶片白板挿入MASK。<br>PKG品的排整列移載也可対応。</div>'>
+              <figure class="lineup__item__img">
+                <img src="<?php echo AKIM_IMG_URI; ?>products/line-slider-8.png" width="200" alt="">
+                <figcaption class="lineup__item__name">晶片上片機</figcaption>
+              </figure>
+            </a>
+          </div>
+          <div class="lineup__item">
+            <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/line-slider-1-modal.png"
+              data-caption='<div class="fancybox-caption__index">
+                                晶片點膠機
+                            </div><div class="fancybox-caption__description">W1700☓D1400☓H1700<br>陶瓷基座搭載點膠晶片。</div>'>
+              <figure class="lineup__item__img">
+                <img src="<?php echo AKIM_IMG_URI; ?>products/line-slider-1.png" width="200" alt="">
+                <figcaption class="lineup__item__name">晶片點膠機</figcaption>
+              </figure>
+            </a>
+          </div>
+          <div class="lineup__item">
+            <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/line-slider-2-modal.png"
+              data-caption='<div class="fancybox-caption__index">
+                                清洁硬化路
+                            </div><div class="fancybox-caption__description">W2800☓D1400☓H1700<br>在氮氣中加熱和固化粘合劑。<br>炉子清洁度为100级或更高。</div>'>
+              <figure class="lineup__item__img">
+                <img src="<?php echo AKIM_IMG_URI; ?>products/line-slider-2.png" width="200" alt="">
+                <figcaption class="lineup__item__name">清洁硬化路</figcaption>
+              </figure>
+            </a>
+          </div>
+          <div class="lineup__item">
+            <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/line-slider-3-modal.png"
+              data-caption='<div class="fancybox-caption__index">
+                                微調上載機/下載機
+                            </div><div class="fancybox-caption__description">W2300☓D1300☓H1700<br>搬送Carrier上PKG移載到托盤上。<br>Carrier向微調機搬送。</div>'>
+              <figure class="lineup__item__img">
+                <img src="<?php echo AKIM_IMG_URI; ?>products/line-slider-3.png" width="200" alt="">
+                <figcaption class="lineup__item__name">微調上載機/下載機</figcaption>
+              </figure>
+            </a>
+          </div>
+          <div class="lineup__item">
+            <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/line-slider-4-modal.png"
+              data-caption='<div class="fancybox-caption__index">
+                                LID預焊機
+                            </div><div class="fancybox-caption__description">W1200☓D1000☓H1700<br>LID預焊到PKG上。</div>'>
+              <figure class="lineup__item__img">
+                <img src="<?php echo AKIM_IMG_URI; ?>products/line-slider-4.png" width="200" alt="">
+                <figcaption class="lineup__item__name">LID預焊機</figcaption>
+              </figure>
+            </a>
+          </div>
+          <div class="lineup__item">
+            <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/line-slider-5-modal.png"
+              data-caption='<div class="fancybox-caption__index">
+                                真空退火設備
+                            </div><div class="fancybox-caption__description">W1100☓D1300☓H1700<br>封焊止PKG在真空中進行退火処理。</div>'>
+              <figure class="lineup__item__img">
+                <img src="<?php echo AKIM_IMG_URI; ?>products/line-slider-5.png" width="200" alt="">
+                <figcaption class="lineup__item__name">真空退火設備</figcaption>
+              </figure>
+            </a>
+          </div>
+          <div class="lineup__item">
+            <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/line-slider-6-modal.png"
+              data-caption='<div class="fancybox-caption__index">
+                                直交真空封焊设备
+                            </div><div class="fancybox-caption__description">W1700☓D1200☓H1700<br>真空中PKG的両辺進行封焊。</div>'>
+              <figure class="lineup__item__img">
+                <img src="<?php echo AKIM_IMG_URI; ?>products/line-slider-6.png" width="200" alt="">
+                <figcaption class="lineup__item__name">直交真空封焊设备</figcaption>
+              </figure>
+            </a>
+          </div>
+          <div class="lineup__item">
+            <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/line-slider-7-modal.png"
+              data-caption='<div class="fancybox-caption__index">
+                                選別設備
+                            </div><div class="fancybox-caption__description">W800☓D900☓H1700<br>從搬送治具中對產品進行良品・不良品的分類取出。</div>'>
+              <figure class="lineup__item__img">
+                <img src="<?php echo AKIM_IMG_URI; ?>products/line-slider-7.png" width="200" alt="">
+                <figcaption class="lineup__item__name">選別設備</figcaption>
+              </figure>
+            </a>
+          </div>
+          <div class="lineup__item">
+            <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/line-slider-9-modal.png"
+              data-caption='<div class="fancybox-caption__index">
+                                温度特性检查设备
+                            </div><div class="fancybox-caption__description">W1000☓D1000☓H1700<br>對晶體進行電気特性検査、激光印字及編帯。</div>'>
+              <figure class="lineup__item__img">
+                <img src="<?php echo AKIM_IMG_URI; ?>products/line-slider-9.png" width="200" alt="">
+                <figcaption class="lineup__item__name">温度特性检查设备</figcaption>
+              </figure>
+            </a>
+          </div>
+          <div class="lineup__item">
+            <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/line-slider-10-modal.png"
+              data-caption='<div class="fancybox-caption__index">
+                                常温検査・打標・編帯設備
+                            </div><div class="fancybox-caption__description">W1200☓D1000☓H1700<br>對晶體進行電気特性検査、激光印字及編帯。</div>'>
+              <figure class="lineup__item__img">
+                <img src="<?php echo AKIM_IMG_URI; ?>products/line-slider-10.png" width="200" alt="">
+                <figcaption class="lineup__item__name">常温検査・打標・編帯設備</figcaption>
+              </figure>
+            </a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </section>
+  <!-- /////  -->
+</main>
 <?php
     }
 ?>
