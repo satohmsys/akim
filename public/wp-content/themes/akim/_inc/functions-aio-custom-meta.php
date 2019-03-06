e1b100e7db67d2aa54db9ee027224338b2ae414b
佐藤正康 <satohmsys@gmail.com>
2019-03-06 11:06:17 +0900

    php 表示中の言語に合わせ、カスタムタクソノミーアーカイブページのdescriptionを変えるAll In One SEO上書き用処理

--- public/wp-content/themes/akim/_inc/functions-aio-custom-meta.php
+++ public/wp-content/themes/akim/_inc/functions-aio-custom-meta.php
@@ -9,6 +9,32 @@ function my_description($description){
   if(is_post_type_archive('products') ){
     $description = __('AKIM株式会社の製品紹介ページです。', 'akim_tr');
   }
+
+  if( is_tax() ){
+    // 閲覧中の言語をURIから判断。
+    $currentPage = $_SERVER["REQUEST_URI"];
+    preg_match("/ko|kr|en|ch/",$currentPage, $result);
+    $selectedLang = $result ? $result[0] : 'ja';
+
+    // AKIM株式会社の【商品名】を紹介します　の形式なので
+    // 「AKIM株式会社」 / 「を紹介します」変数をそれぞれの言語で作成
+      $description_prefix_before = 'AKIM株式会社の';
+      $description_prefix_after = 'を紹介します。';
+
+    if($selectedLang == 'ko'||$selectedLang == 'kr'){
+      $description_prefix_before = 'AKIM주식회사의 ';
+      $description_prefix_after = ' 소개합니다.';
+    } elseif ($selectedLang == 'ch') {
+      $description_prefix_before = '介紹AKIM株式会社的';
+      $description_prefix_after = '';
+    } elseif ($selectedLang == 'en') {
+      $description_prefix_before = 'Introduction to ';
+      $description_prefix_after = ' of AKIM Corporation.';
+    }    
+    $description = $description_prefix_before . single_term_title( false, false ) . $description_prefix_after;
+  }
+
+
   return $description;
 }
 add_filter('aioseop_description', 'my_description');
