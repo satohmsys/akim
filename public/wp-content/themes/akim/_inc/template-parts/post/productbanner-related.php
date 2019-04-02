b3c0c6a488f8bc4ae3c35387f14e825a5f644961
佐藤正康 <satohmsys@gmail.com>
2019-04-02 12:24:51 +0900

    商品カテゴリ関連リンク（フッター部分　修正

--- public/wp-content/themes/akim/_inc/template-parts/post/productbanner-related.php
+++ public/wp-content/themes/akim/_inc/template-parts/post/productbanner-related.php
@@ -1,6 +1,6 @@
 <ul class="productBanner relatedProduct">
 <li class="productBanner__item relatedProduct__item">
-    <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/detail-link-suisho-modal.png" data-caption='<div class="fancybox-caption__index">
+    <a data-fancybox href="<?php echo AKIM_LOCALIMG_URI; ?>products/detail-link-suisho-modal.png" data-caption='<div class="fancybox-caption__index">
             <?php echo __('水晶振動子外観・構造', 'akim_tr'); ?>
         </div>'>							
         <figure class="relatedProduct__item__img">
@@ -12,7 +12,7 @@
     </a>
 </li>
 <li class="productBanner__item relatedProduct__item">
-    <a data-fancybox href="<?php echo AKIM_IMG_URI; ?>products/detail-link-crumpcareer-modal.png" data-caption='<div class="fancybox-caption__index">
+    <a data-fancybox href="<?php echo AKIM_LOCALIMG_URI; ?>products/detail-link-crumpcareer-modal.png" data-caption='<div class="fancybox-caption__index">
             <?php echo __('<p>トレイ</p><p>クランプキャリア<br><strong>特許番号</strong>','akim_tr'); ?>&nbsp;4895061,6068281</p></div>'>							
         <figure class="relatedProduct__item__img">
             <img src="<?php echo AKIM_IMG_URI; ?>products/detail-link-crumpcareer.jpg" alt="">
